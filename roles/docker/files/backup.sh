e6b13f6b5582c6143760020dac0bdce0165169a4
jincod <vadim.abdrashitov@gmail.com>
2017-02-26 15:16:10 +0500

    updated backup script

--- roles/docker/files/backup.sh
+++ roles/docker/files/backup.sh
@@ -1,24 +1,11 @@
-S3KEY="my aws key"
-S3SECRET="my aws secret"
+TIMESTAMP=date +%Y-%m-%d-%H%M%S
+FILE_NAME=$TIMESTAMP.tar.gz
+BUCKET_NAME=
 
-function putS3
-{
-  path=$1
-  file=$2
-  aws_path=$3
-  bucket='my-aws-bucket'
-  date=$(date +"%a, %d %b %Y %T %z")
-  acl="x-amz-acl:public-read"
-  content_type='application/x-compressed-tar'
-  string="PUT\n\n$content_type\n$date\n$acl\n/$bucket$aws_path$file"
-  signature=$(echo -en "${string}" | openssl sha1 -hmac "${S3SECRET}" -binary | base64)
-  curl -X PUT -T "$path/$file" \
-    -H "Host: $bucket.s3.amazonaws.com" \
-    -H "Date: $date" \
-    -H "Content-Type: $content_type" \
-    -H "$acl" \
-    -H "Authorization: AWS ${S3KEY}:$signature" \
-    "https://$bucket.s3.amazonaws.com$aws_path$file"
-}
+# Backup
+docker run --rm --volume jenkins_jenkins-data:/jenkins_data --volume /var/backups/jenkins:/backup ubuntu tar cvzf /backup/$FILE_NAME /jenkins_data --exclude=/jenkins_data/workspace
 
-docker run --rm --volumes-from jenkins-data-container -v $(pwd):/backup jenkins:latest tar cvf /backup/backup.tar /var/jenkins_home
+# Upload to s3
+aws s3 cp /var/backups/jenkins/$FILE_NAME s3://$BUCKET_NAME/$FILE_NAME
+
+# Clear old backups
