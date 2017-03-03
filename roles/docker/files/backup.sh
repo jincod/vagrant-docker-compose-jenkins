64f145db0bad4c6ff9b3bafb5d04e43b756285ad
jincod <vadim.abdrashitov@gmail.com>
2017-03-03 15:02:23 +0500

    updated backup script

--- roles/docker/files/backup.sh
+++ roles/docker/files/backup.sh
@@ -1,9 +1,9 @@
-TIMESTAMP=date +%Y-%m-%d-%H%M%S
-FILE_NAME=$TIMESTAMP.tar.gz
-BUCKET_NAME=
+TIMESTAMP=$(date +%Y-%m-%d-%H%M%S)
+FILE_NAME=backup-$TIMESTAMP.tar.gz
+BUCKET_NAME=your-bucket-name
 
 # Backup
-docker run --rm --volume jenkins_jenkins-data:/jenkins_data --volume /var/backups/jenkins:/backup ubuntu tar cvzf /backup/$FILE_NAME /jenkins_data --exclude=/jenkins_data/workspace
+docker run --rm --volume jenkins_jenkins-data:/jenkins_data --volume /var/backups/jenkins:/backup ubuntu tar cvzf /backup/$FILE_NAME /jenkins_data --exclude=/jenkins_data/workspace --exclude=/jenkins_data/caches
 
 # Upload to s3
 aws s3 cp /var/backups/jenkins/$FILE_NAME s3://$BUCKET_NAME/$FILE_NAME
